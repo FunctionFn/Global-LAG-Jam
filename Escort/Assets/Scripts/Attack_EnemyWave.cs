e0dab73bc49d91bbeae0a2f03731d9910917e6d4
anoldlady9 <DCHENG5@depaul.edu>
2017-01-21 14:48:53 -0600

    Added Slam Fish and Wave behavior

---
+++ Escort/Assets/Scripts/Attack_EnemyWave.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack_EnemyWave : MonoBehaviour {
+
+    public int waveMaxTime = 50;
+
+    public int waveTimer = 0;
+
+    private bool activeWave = false;
+  
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartWave();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        waveTimer++;
+
+        // Is wave done?
+        if (waveTimer >= waveMaxTime)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    public void StartWave (/*Vector3 pos*/)
+    {
+        waveTimer = 0;
+    }
+}
