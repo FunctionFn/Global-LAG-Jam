f1bd69b5934e05fb1616bff167539b5821925f74
anoldlady9 <DCHENG5@depaul.edu>
2017-01-21 15:20:59 -0600

    Added Crabs

---
+++ Escort/Assets/Scripts/Enemy_Crab.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Crab : MonoBehaviour {
+
+    public GameObject target;
+
+    public float speed = 0.025f;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Move towards target
+        Vector3 targetDir = target.transform.position - transform.position;
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+        transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, targetDir, speed, 0.0F));
+    }
+}
